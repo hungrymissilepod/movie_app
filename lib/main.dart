f5e714aa6f72b0beac3562d10aa6c0cb6290303b
Jake King-Lee <jake.kinglee@gmail.com>
2021-05-17 15:29:44 +0100

    Cleaned up code. Added comments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' show json;
 
-import 'package:http/http.dart' as http;
-import 'package:movieapp/utilities/size_config.dart';
+/// Utilities
 import 'package:movieapp/utilities/ui_constants.dart';
+import 'package:movieapp/utilities/size_config.dart';
+import 'package:http/http.dart' as http;
 
-import 'dart:convert';
+/// Models
+import 'package:movieapp/models/movie_model.dart';
 
-// TODO: add movie reel image at top of sliver app bar (in red)
-// TODO: create splash screen. Show splash screen on app launch
-// TODO: ensure app scales correctly on all devices
-// TODO: clean up code
-// TODO: comment code
-
-class Movie {
-  Movie({ this.title, this.voteAverage, this.imageUrl });
-
-  final String title;
-  final double voteAverage;
-  final String imageUrl;
-
-  /// Convert JSON to Movie object
-  factory Movie.fromJson(Map data) {
-    return Movie(
-      title: data['title'] as String ?? '',
-      voteAverage: data["vote_average"] is int ? (data['vote_average'] as int).toDouble() : data['vote_average'],
-      imageUrl: data['poster_path'] as String ?? '',
-    );
-  }
-}
+/// Widgets
+import 'package:movieapp/widgets/movie_poster_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -55,6 +38,7 @@ class MyApp extends StatelessWidget {
         ),
         primarySwatch: Colors.red,
       ),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -68,23 +52,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  /// ScrollController so we can detect when user is near the bottom of the screen
   ScrollController _controller = ScrollController();
+
+  /// Future for FutureBuilder so we can wait until movies have loaded
+  Future _future;
+
+  /// List of [Movie] objects we will display in scroll view
+  List<Movie> _movies = [];
+  
+  /// The current page of data we are loading from API
   int _page = 0;
 
-  Future _future;
-  List<Movie> _movies = [];
+  /// Are we currently loading data from the API? Helps reduce the amount of data loaded at once
   bool _isLoading = false;
 
   Future getMovies() async {
-    setState(() { _isLoading = true; });
-    _page++;
+    setState(() { _isLoading = true; _page++; });
     print('getMovies - page: $_page');
+
+    /// Get movie data for this [_page]
     var url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=4418a4dd7da6409b55bd0876c7540a10&language=en-US&page=$_page');
     var response = await http.get(url);
-    // print(response.body);
-
+    
+    /// Deserialise reponse body to json
     final body = json.decode(response.body);
+    /// Convert json to list of movies and add them to [_movies] list
     _movies.addAll((body['results'] as List).map((e) => Movie.fromJson(e as Map<String, dynamic>)).toList());
+
     setState(() { _isLoading = false; });
     return _movies;
   }
@@ -112,63 +108,73 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
+    double gridPadding = SizeConfig.blockSizeHorizontal * 3;
     return Scaffold(
       body: FutureBuilder(
         future: _future,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return CustomScrollView(
-              controller: _controller,
-              slivers: [
-                SliverAppBar(
-                  backgroundColor: Color(kBackgroundColour),
-                  toolbarHeight: 200,
-                  title: Container(
-                    child: Column(
-                      children: [
-                        Text(
-                          'REEL DEALS',
-                          style: TextStyle(
-                            fontFamily: 'CFParis',
-                            color: Color(kAccentColour),
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4,
-                            fontSize: 80,
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: gridPadding),
+              child: CustomScrollView(
+                physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                controller: _controller,
+                slivers: [
+                  SliverAppBar(
+                    brightness: Brightness.dark,
+                    backgroundColor: Color(kBackgroundColour),
+                    toolbarHeight: SizeConfig.blockSizeVertical * 22,
+                    title: Container(
+                      child: Column(
+                        children: [
+                          Image(
+                            image: AssetImage('assets/movie_reel.png'),
+                            height: SizeConfig.blockSizeVertical * 6,
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          'Find the best movie deals for you',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22,
+                          Text(
+                            'REEL DEALS',
+                            style: TextStyle(
+                              fontFamily: 'CFParis',
+                              color: Color(kAccentColour),
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4,
+                              fontSize: 70,
+                            ),
                           ),
-                        ),
-                      ],
+                          SizedBox(height: 10),
+                          Text(
+                            'Find the best movie deals for you',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SliverGrid(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1/1.5,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 14,
+                  SliverGrid(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1/1.5,
+                      crossAxisSpacing: gridPadding,
+                      mainAxisSpacing: gridPadding,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return MoviePoster(
+                          _movies[index].title,
+                          _movies[index].voteAverage,
+                          _movies[index].imageUrl,
+                        );
+                      },
+                      childCount: _movies.length,
+                    ),
                   ),
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      return MoviePoster(
-                        _movies[index].title,
-                        _movies[index].voteAverage,
-                        _movies[index].imageUrl,
-                      );
-                    },
-                    childCount: _movies.length,
-                  ),
-                ),
-              ],
+                ],
+              ),
             );
           }
           return Center(child: CircularProgressIndicator());
@@ -191,81 +197,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class MoviePoster extends StatelessWidget {
-  MoviePoster(this.title, this.vote, this.imageUrl);
-  final String title;
-  final double vote;
-  final String imageUrl;
-
-  @override
-  Widget build(BuildContext context) {
-    double stars = vote / 2; /// convert vote score to number of stars
-    return Stack(
-      fit: StackFit.passthrough,
-      children: [
-        Container(
-          child: FittedBox(
-            fit: BoxFit.fill,
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              child: Image.network('https://image.tmdb.org/t/p/w500/$imageUrl')),
-          ),
-        ),
-        Positioned(
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            clipBehavior: Clip.none,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0)),
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [ Colors.transparent, Colors.black87.withOpacity(0.35), Colors.black87 ],
-              )
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: SizeConfig.blockSizeVertical * 3, horizontal: SizeConfig.blockSizeHorizontal * 2),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: SizeConfig.blockSizeVertical * 1),
-                  Row(
-                    children: List.generate(5, (index) {
-                      IconData icon = Icons.star_outline_rounded; /// default to empty star
-                      /// If this movie has more stars than [index+1], show a full star
-                      if (stars > index+1) {
-                        icon = Icons.star_rounded;
-                        /// check if this movie can get half a star. Check if stars are more than [index] and that stars is greater by 0.5, show a half star
-                      } else if (stars > index && (stars - index) >= 0.5) {
-                        icon = Icons.star_half_rounded;
-                      }
-                      return Icon(icon,  size: SizeConfig.blockSizeVertical * 3);
-                    }),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
